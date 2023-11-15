b06b51aec570cb2c1bc5bd0a5a41bfb055891712
zikkri <zbowker66@gmail.com>
2023-11-15 14:57:57 -0500

    fixed everything

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,8 +1,6 @@
-import React from 'react';
-import {TiDelete} from 'react-icons/ti';
+import React, { useContext } from 'react';
+import { TiDelete } from 'react-icons/ti';
 import { AppContext } from '../context/AppContext';
-import { useContext } from 'react';
-
 
 const ExpenseItem = (props) => {
     const { dispatch } = useContext(AppContext);
@@ -10,30 +8,31 @@ const ExpenseItem = (props) => {
     const handleDeleteExpense = () => {
         dispatch({
             type: 'DELETE_EXPENSE',
-            payload: props.id
+            payload: props.id,
         });
     };
 
-    const increaesAllocation = (name) => {
+    const increaseAllocation = (name) => {
         const expense = {
             name: name,
-            cost: 10
+            cost: 10,
         };
 
         dispatch({
             type: 'ADD_EXPENSE',
-            cost: 10
-        });        
+            payload: expense
+        });
+
     }
 
     return (
         <tr>
-            <td>{props.name}</td>
-            <td>£{props.cost}</td>
-            <td><button onClick={event => increaesAllocation(props.name)}>+</button></td>
-            <td><TiDelete size="1.5rem" onClick={handleDeleteExpense}></TiDelete></td>            
+        <td>{props.name}</td>
+        <td>£{props.cost}</td>
+        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
+        <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
     );
-}
+};
 
 export default ExpenseItem;
--- src/components/Remaining.js
+++ src/components/Remaining.js
@@ -1,20 +1,15 @@
 import React, { useContext } from 'react';
-import { AppContext} from '../context/AppContext';
-
+import { AppContext } from '../context/AppContext';
 const Remaining = () => {
     const { expenses, budget } = useContext(AppContext);
     const totalExpenses = expenses.reduce((total, item) => {
-        return (total = total + item.cost)
-    });
-
+        return (total = total + item.cost);
+    }, 0);
     const alertType = totalExpenses > budget ? 'alert-danger' : 'alert-success';
-
     return (
         <div className={`alert ${alertType}`}>
-            <span>Remaining: £{ budget - totalExpenses }</span>
+            <span>Remaining: £{budget - totalExpenses}</span>
         </div>
     );
 };
-
-
 export default Remaining;
